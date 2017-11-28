a97d11834973e4d542b306744c856c67e63ade7b
Jeonghwan <Jeonghwan@DESKTOP-QN9SVRB>
2017-11-28 22:25:04 +0900

    Nodes interaction added

--- TermProject_prototype/src/Main.java
+++ TermProject_prototype/src/Main.java
@@ -5,27 +5,45 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		// create Node1
-		Node n1 = new Node("node1");
-		// create blockchain
-		n1.createBlockChain("testChain");
-		// create transactions
-		n1.getBlockChain("testChain").createTransaction("aaaa");
-		n1.getBlockChain("testChain").createTransaction("bbbb");
-		// create new block
-		Block newBlock = n1.getBlockChain("testChain").createBlock();
-		// add created block to blockchain
-		n1.getBlockChain("testChain").addBlock(newBlock);
-		// print blockchain status to console
-		n1.getBlockChain("testChain").showBlockChain();
-
-		// create transactions
-		n1.getBlockChain("testChain").createTransaction("tx123");
-		n1.getBlockChain("testChain").createTransaction("tx567");
-		// create new block and add created block to blockchain
-		n1.getBlockChain("testChain").addBlock(n1.getBlockChain("testChain").createBlock());
-		// print blockchain status to console
-		n1.getBlockChain("testChain").showBlockChain();
-
+		Node n1 = new Node("node1", null);
+		
+		//create transactions
+		n1.createTransaction("aaaaa");
+		n1.createTransaction("bbbbb");
+		//create block and add it to block chain
+		n1.updateBlockChain();
+		//print block chain
+		n1.showBlockChain();
+		
+		
+		//create another nodes and do things
+		Node n2 = new Node("node2", n1);
+		n2.addPeer(n1);
+		
+		n2.createTransaction("ccccc");
+		n2.updateBlockChain();
+		n2.createTransaction("ddddd");
+		
+		Node n3 = new Node("node3", n2);
+		n3.addPeer(n1);
+		n3.addPeer(n2);
+		n3.deletePeer(n2);
+		n3.createTransaction("eeeee");
+		n3.createTransaction("fffff");
+		n3.updateBlockChain();
+		
+		
+		//print all
+		n1.showBlockChain();
+		n2.showBlockChain();
+		n3.showBlockChain();
+		
+		
+		Node n4 = new Node("node4", n1);
+		n4.showBlockChain();
+		
+		
+		
 	}
 
 }
--- TermProject_prototype/src/Node.java
+++ TermProject_prototype/src/Node.java
@@ -1,16 +1,17 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Node {
 	private String nodeName;
-	private Map<String, BlockChain> blockChainMap;
+	private BlockChain blockChain;
 	private List<Node> peers;
 
-	public Node(String nodeName) {
+	public Node(String nodeName, Node downloadFrom) {
 		this.nodeName = nodeName;
-		blockChainMap = new HashMap<String, BlockChain>();
+		blockChain = new BlockChain();
+		if (downloadFrom != null) {
+			blockChain.downloadBlockChain(downloadFrom.getBlockChain());
+		}
 		peers = new ArrayList<Node>();
 	}
 
@@ -18,16 +19,77 @@ public class Node {
 		return nodeName;
 	}
 
-	public BlockChain getBlockChain(String blockChainName) {
-		return blockChainMap.get(blockChainName);
+	public BlockChain getBlockChain() {
+		return blockChain;
 	}
 
-	public void createBlockChain(String blockChainName) {
-		if (blockChainMap.containsKey(blockChainName)) {
-			System.out.println("already used name");
-		} else {
-			blockChainMap.put(blockChainName, new BlockChain(blockChainName));
+	public List<Node> getPeers() {
+		return peers;
+	}
+
+	public void createTransaction(String data) {
+		Transaction tx = blockChain.createTransaction(data);
+		for (int i = 0; i < peers.size(); i++) {
+			peers.get(i).receiveTransaction(this, tx);
 		}
 	}
 
+	private void receiveTransaction(Node senderNode, Transaction tx) {
+		if (blockChain.getTransaction(tx.getHash()) == null) {
+			blockChain.addTransaction(tx);
+			for (int i = 0; i < peers.size(); i++) {
+				if (peers.get(i) != senderNode) {
+					peers.get(i).receiveTransaction(this, tx);
+				}
+			}
+		}
+	}
+
+	private Block createBlock() {
+		return blockChain.createBlock();
+	}
+
+	private void addBlock(Block newBlock) {
+		blockChain.addBlock(newBlock);
+
+		for (int i = 0; i < peers.size(); i++) {
+			peers.get(i).receiveBlock(this, BlockChain.cloneBlock(newBlock));
+		}
+	}
+
+	private void receiveBlock(Node senderNode, Block newBlock) {
+		if (blockChain.getBlock(newBlock.getHash()) == null) {
+			blockChain.addBlock(newBlock);
+
+			for (int i = 0; i < peers.size(); i++) {
+				if (peers.get(i) != senderNode) {
+					peers.get(i).receiveBlock(this, newBlock);
+				}
+			}
+		}
+	}
+
+	public void showBlockChain() {
+		System.out.println();
+		System.out.println(nodeName);
+		blockChain.showBlockChain();
+	}
+
+	public void updateBlockChain() {
+		if (blockChain.getTxsNotYetAddedSize() > 0) {
+			this.addBlock(this.createBlock());
+		}
+	}
+
+	public void addPeer(Node peer) {
+		if (!peers.contains(peer)) {
+			peers.add(peer);
+			peer.addPeer(this);
+		}
+	}
+
+	public boolean deletePeer(Node peer) {
+		peer.getPeers().remove(this);
+		return peers.remove(peer);
+	}
 }
